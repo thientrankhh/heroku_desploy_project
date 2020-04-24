b3d9568a0abfac88248fdcb844c59979d2c3a58e
thientrankhh <thientrankhh@gmail.com>
2020-04-24 13:06:24 +0700

    Create Migration and Seeder

---
+++ database/migrations/2020_04_23_093439_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('order_id');
+            $table->decimal('total_price',10,1);
+            $table->string('customer_name');
+            $table->string('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('order_notes');
+            $table->dateTime('order_date');
+            $table->boolean('order_status')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
---
+++ database/seeds/OrdersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OrdersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       $data = [
+           [
+                'total_price' => '200',
+                'customer_name' => 'Thien',
+                'address' => 'Da Nang City',
+                'phone' => '0975552480',
+                'email' => 'thientrankhh@gmail.com',
+                'order_notes' => '',
+                'order_date' => '2020-04-24',
+                'order_status' => '0'
+           ]
+       ];
+
+       DB::table('orders')->insert($data);
+    }
+}
